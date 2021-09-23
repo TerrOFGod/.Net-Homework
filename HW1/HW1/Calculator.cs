0dd4967b7f3be23e50c7692b9b6b1d0dde575191
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2021-09-23 19:51:45 +0300

    restore HW1

---
+++ HW1/HW1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace HW1
+{
+    public static class Calculator
+    {
+        public static int Calculate(Operations operation, int val1, int val2)
+        {
+            var result = operation switch
+            {
+                Operations.Plus => val1 + val2,
+                Operations.Minus => val1 - val2,
+                Operations.Multiply => val1 * val2,
+                _ => val1 / val2,
+            };
+
+            return result;
+        }
+    }
+}
