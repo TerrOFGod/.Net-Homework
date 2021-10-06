f0ccc32333228052d465a7d0c19f2f2b7353b7a9
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2021-10-06 10:46:09 +0300

    update
    delete wrong tests, edit calculator, add xml and edit yml

--- HW4/HW4/Calculator.fs
+++ HW4/HW4/Calculator.fs
@@ -4,7 +4,7 @@ let calculate operation val1 val2 =
     let xd = val1 |> double
     let yd = val2 |> double
     match operation with
-    | Plus -> xd + yd
-    | Minus -> xd - yd
-    | Multiply -> xd * yd
+    | Operations.Plus -> xd + yd
+    | Operations.Minus -> xd - yd
+    | Operations.Multiply -> xd * yd
     | _ -> xd / yd
