2a0da8353e7f7b79920fbc6c01ca49f142526699
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2021-12-09 21:32:09 +0300

    rename and add hw1-hw5

---
+++ HW5/HW5/ResultBuilder.fs
@@ -0,0 +1,14 @@
+﻿module HW5.ResultBuilder
+
+type ResultBuilder() =
+    member this.Zero() = Error "Arguments have error(s)"
+
+    member this.Bind(x, f) =
+        match x with    
+        | Ok x -> f x
+        | Error error -> Error error
+        
+    member this.Return x = Ok x
+
+let result = ResultBuilder()
+    
