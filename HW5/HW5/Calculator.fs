f268c32b7a58383579a8c8ef44c10b278400616a
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2021-12-09 21:36:08 +0300

    rename sln and add hw2-hw5

---
+++ HW5/HW5/Calculator.fs
@@ -0,0 +1,17 @@
+﻿module HW5.Calculator
+
+open HW5.ResultBuilder
+
+let divideBy numerator denominator =
+    if denominator = decimal 0 then Error "Denominator is zero(0)"
+    else Ok(numerator / denominator)
+    
+let calculate (val1, operation, val2) =
+    result{
+        match operation with
+        | Operations.Plus -> return val1 + val2
+        | Operations.Minus -> return val1 - val2
+        | Operations.Multiply -> return val1 * val2
+        | _ -> let! res =  divideBy val1 val2
+               return res
+    }
