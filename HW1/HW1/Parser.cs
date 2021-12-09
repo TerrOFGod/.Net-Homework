39ba926e05c6c6facb5020d8008607703464c23a
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2021-12-09 21:50:44 +0300

    hw1

---
+++ HW1/HW1/Parser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace HW1
+{
+    public static class Parser
+    {
+        private const int Correct = 0;
+        private const int WrongArgCount = 1;
+        private const int WrongArgFormat = 2;
+        private const int WrongOperation = 3;
+
+        public static int TryParseArguments(string[] args, out Operations operation, out int val1, out int val2)
+        {
+            operation = default;
+            val1 = default;
+            val2 = default;
+            
+            if (!CheckArgumentsCount(args))
+                return WrongArgCount;
+            
+            if (!TryParseOperation(args[1], out operation))
+                return WrongOperation;
+            
+            if (!(TryParseValue(args[0], out val1) && TryParseValue(args[2], out val2)))
+            {
+                return WrongArgFormat;
+            }
+            
+            return Correct;
+        }
+        
+        private static bool TryParseValue(string args, out int val)
+        {
+            if (int.TryParse(args, out val)) 
+                return true;
+            Console.WriteLine($"Value is not int: {args}");
+            return false;
+        }
+
+        private static bool TryParseOperation(string arg, out Operations operation)
+        {
+            var temp = arg switch
+            {
+                "+" => 0,
+                "-" => 1,
+                "*" => 2,
+                "/" => 3,
+                _ => 4
+            };
+            if (!(temp < 4))
+            {
+                operation = default;
+                Console.WriteLine($"The calculator does not recognize this operation: {arg}");
+                return false;
+            }
+
+            operation = (Operations) temp;
+            return true;
+        }
+
+        private static bool CheckArgumentsCount(string[] args)
+        {
+            if (args.Length == 3) return true;
+            Console.WriteLine($"The program requires 3 arguments to work, but the {args.Length} was entered");
+            return false;
+        }
+    }
+}
---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW1
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var isValid = Parser.TryParseArguments(args, out var operation,
+                out var val1, out var val2);
+            if (isValid != 0) return isValid;
+            
+            var result = Calculator.Calculate(operation, val1, val2);
+            Console.WriteLine($"Result is: {result}");
+            return 0;
+        }
+    }
+}
