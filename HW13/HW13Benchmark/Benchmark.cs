f0b5ec5f4ec853cdc387291deae4bf57a05fc30a
TerrOFGod <83634610+TerrOFGod@users.noreply.github.com>
2021-12-23 10:46:26 +0300

    update
    add HW13 benchmark

---
+++ HW13/HW13Benchmark/Benchmark.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Attributes;
+
+namespace HW13Benchmark
+{
+    [MemoryDiagnoser]
+    [MinColumn]
+    [MaxColumn]
+    [MedianColumn]
+    [MeanColumn]
+    [StdDevColumn]
+    public class Benchmark
+    {
+        private Methods methods;
+        private string _testNumber;
+        private static MethodInfo _methodFromTest;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            methods = new Methods();
+            _methodFromTest = typeof(Methods).GetMethod("Reflection");
+            _testNumber = "10";
+        }
+
+        [Benchmark(Description = "simple method")]
+        public void TestSimpleMethod()
+        {
+            methods.Simple(_testNumber);
+        }
+
+        [Benchmark(Description = "static method")]
+        public void TestStaticMethod()
+        {
+            Methods.Static(_testNumber);
+        }
+
+        [Benchmark(Description = "virtual method")]
+        public void TestVirtualMethod()
+        {
+            methods.Virtual(_testNumber);
+        }
+
+        [Benchmark(Description = "generic method")]
+        public void TestGenericMethod()
+        {
+            methods.Generic<string>(_testNumber);
+        }
+
+        [Benchmark(Description = "reflection method")]
+        public void TestReflectionMethod()
+        {
+            _methodFromTest.Invoke(methods, new object[] {_testNumber});
+        }
+
+        [Benchmark(Description = "dynamic method")]
+        public void TestDynamicMethod()
+        {
+            methods.Dynamic(_testNumber);
+        }
+    }
+}
---
+++ HW13/HW13Benchmark/Methods.cs
@@ -0,0 +1,40 @@
+﻿namespace HW13Benchmark
+{
+    public class Methods
+    {
+        public string Simple(string num)
+        {
+            num += num;
+            return num;
+        }
+
+        public virtual string Virtual(string num)
+        {
+            num += num;
+            return num;
+        }
+
+        public static string Static(string num)
+        {
+            num += num;
+            return num;
+        }
+
+        public string Generic<T>(T num)
+        {
+            return num.ToString() + num;
+        }
+
+        public string Dynamic(dynamic num)
+        {
+            num += num.ToString();
+            return num;
+        }
+
+        public string Reflection(string num)
+        {
+            num += num;
+            return num;
+        }
+    }
+}
